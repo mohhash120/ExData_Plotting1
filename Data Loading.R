df20e0a78c708269e147a30d315359a4b8e2f65c
mohhash120 <mohhash120@gmail.com>
2016-02-15 09:58:45 +0300

    Create Data Loading.R

---
+++ Data Loading.R
@@ -0,0 +1,16 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+        temp <- tempfile()
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        file <- unzip(temp)
+        unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+data <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
+data$Voltage <- as.numeric(as.character(data$Voltage))
+data <- transform(data, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
